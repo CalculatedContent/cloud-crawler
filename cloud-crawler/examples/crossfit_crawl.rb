00812bb78fd91e4c7bbaf103ca791daa6eea38a1
Charles H Martin <charlesmartin14@gmail.com>
2013-04-16 15:49:52 -0700

    prettier

--- cloud-crawler/examples/crossfit_crawl.rb
+++ cloud-crawler/examples/crossfit_crawl.rb
@@ -29,7 +29,7 @@ CloudCrawler::crawl(opts[:urls], opts)  do |cc|
   
   cc.focus_crawl do |page|
     page.links.keep_if do |lnk| 
-       text_for(lnk) =~ /Level 1/i
+       text_for lnk =~ /Level 1/i
     end
   end
 
