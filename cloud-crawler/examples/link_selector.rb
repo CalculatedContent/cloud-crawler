5b916326e2b7d6451c58f33ed2381d7255ea5a1b
Charles H Martin <charlesmartin14@gmail.com>
2013-04-18 15:02:21 -0700

    doc fix

--- cloud-crawler/examples/link_selector.rb
+++ cloud-crawler/examples/link_selector.rb
@@ -13,14 +13,15 @@ opts = Trollop::options do
   opt :flush,  "", :short => "-f", :default => true
   opt :max_slice, "", :short => "-m", :default => 10
   opt :save_to_s3, "", :short => "-p", :default => false
-  opt :depth_limit, "limit the depth of the crawl", :short => "-l", :type => :int, :default => 0 # only parse first page, no actual crawl
+  opt :depth_limit, "limit the depth of the crawl", :short => "-l", :type => :int, :default => 1 
   opt :discard_page_bodies, "discard page bodies after processing?",  :short => "-d", :default => true
   opt :skip_query_strings, "skip any link with a query string? e.g. http://foo.com/?u=user ",  :short => "-Q", :default => false
 end
 
 
-# find all level 1 certs on the crossfit main site
-
+# Crawl digital camera listings on the first page of eBay
+#  select links by  xpath
+#  
 CloudCrawler::crawl(opts[:urls], opts)  do |cc|
   
   cc.focus_crawl do |page|
@@ -28,10 +29,6 @@ CloudCrawler::crawl(opts[:urls], opts)  do |cc|
       puts "lnk -->  #{lnk}"
     end
   end
-
-   cc.on_every_page do |page|
-    # puts page.url.to_s
-   end
   
 end
 
