cd186fa5ec06df60cd8c67bbbb62cbbd1726204a
Charles H Martin <charlesmartin14@gmail.com>
2013-05-13 12:38:01 -0700

    whenever --set wants @vars, not ENV vars
    fixed worker_schedule.rb

--- cloud-crawler/config/worker_schedule.rb
+++ cloud-crawler/config/worker_schedule.rb
@@ -2,9 +2,10 @@ cc_dir = "/home/ubuntu/apps/cloud-crawler"
 bin_dir = "#{cc_dir}/bin"
 log_dir = "#{cc_dir}/logs"
 
-master_ip = ENV['MASTER_IP_ADDRESS']
+# @master_ip_address is set on the command line by whenever in the start_worker recipe
+# otherwise the default should be the local host
 
 every :reboot do
-   command "cd #{cc_dir}; sudo -E bundle exec  #{bin_dir}/run_worker.rb -h #{master_ip}", :output => "#{log_dir}/worker.log"
+   command "cd #{cc_dir}; sudo -E bundle exec  #{bin_dir}/run_worker.rb -h #{@master_ip_address}", :output => "#{log_dir}/worker.log"
 end
 
