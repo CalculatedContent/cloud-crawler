daa5c8c5ed0e8edbfda3bd8cdb61791ff4d6cdbd
Charles H Martin <charlesmartin14@gmail.com>
2013-05-16 00:30:13 -0700

    renamed logger

--- cloud-crawler/lib/cloud-crawler/logger.rb
+++ cloud-crawler/lib/cloud-crawler/logger.rb
@@ -2,10 +2,6 @@ require 'logger'
 
 #TODO:  emulate rails logging
 module CloudCrawler
-  class Error < ::StandardError
-    attr_accessor :wrapped_exception
-  end
-  
   @logger = Logger.new(STDERR)
   
   def logger
